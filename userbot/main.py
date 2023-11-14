6a83d7bca17d4e2b917f0d2d71f117a4477c585e
Oleksii Pavliuk <capetruha@gmail.com>
2023-11-14 23:55:21 +1100

    Userbot targeted on new york channel

--- userbot/main.py
+++ userbot/main.py
@@ -1,12 +1,17 @@
 from pyrogram import filters
-from config import app,TARGET,TEST_CHAT
+from config import app,TARGET,TEST_CHAT,WORK_NEW_YORK
 
 print('Started!')
 @app.on_message(filters.channel)
 def HandleMessage (client,message):
-  if message.sender_chat and message.sender_chat.id and str(message.sender_chat.id) == TARGET:
+  print(message)
+  if message.sender_chat and message.sender_chat.id and message.sender_chat.id == TARGET and message.text:
+    print('match')
     if 'http' not in message.text:
-      client.send_message(TEST_CHAT,message.text)
-
-
+      try:
+        client.send_message(WORK_NEW_YORK,message.text)
+      except TypeError or KeyError:
+        print('wrong key')
+        print(message)
+        print('------------------------------')
 app.run()
