228c1c056d7cf7c110923f7d2dee9d1fa0e9ae81
Oleksii Pavliuk <capetruha@gmail.com>
2023-11-16 08:35:58 +1100

    Datetime handling in logs

--- userbot/main.py
+++ userbot/main.py
@@ -1,33 +1,36 @@
 from pyrogram import filters
 from config import app,TARGET,TEST_CHAT,WORK_NEW_YORK
+from datetime import datetime
 
-print('Started!')
+print("Started!")
 @app.on_message(filters.channel)
 def HandleMessage (client,message):
-  if message.sender_chat and message.sender_chat.id and message.sender_chat.id == TARGET:
-    print('match')
+  if message.sender_chat and message.sender_chat.id and message.sender_chat.id == TEST_CHAT:
     if message.text:
-      if 'http' not in message.text:
+      if "http" not in message.text and "bazar" not in message.text.lower():
         try:
-          client.send_message(WORK_NEW_YORK,message.text)
+          print("{datetime} ----------------------------- match".format(datetime=datetime.now()))
+          print(message.text)
+          client.send_message(TEST_CHAT,message.text)
         except TypeError or KeyError:
-          print('wrong key')
+          print("{datetime} ------------------------- wrong key".format(datetime=datetime.now()))
           print(message)
-          print('------------------------------')
     elif message.caption and message.photo and message.photo.file_id:
-      if 'http' not in message.caption:
+      if "http" not in message.caption and "bazar" not in message.caption.lower():
         try:
-          app.send_photo(WORK_NEW_YORK,message.photo.file_id,message.caption)
+          print("{datetime} ----------------------------- match".format(datetime=datetime.now()))
+          print(message.caption)
+          app.send_photo(TEST_CHAT,message.photo.file_id,message.caption)
         except TypeError or KeyError:
-          print('wrong key')
+          print("{datetime} ------------------------- wrong key".format(datetime=datetime.now()))
           print(message)
-          print('------------------------------')
     elif message.caption:
-      if 'http' not in message.caption:
+      if "http" not in message.caption and "bazar" not in message.caption.lower():
         try:
-          app.send_message(WORK_NEW_YORK,message.caption)
+          print("{datetime} ----------------------------- match".format(datetime=datetime.now()))
+          print(message.caption)
+          app.send_message(TEST_CHAT,message.caption)
         except TypeError or KeyError:
-          print('wrong key')
+          print("{datetime} ------------------------- wrong key".format(datetime=datetime.now()))
           print(message)
-          print('------------------------------')
 app.run()
